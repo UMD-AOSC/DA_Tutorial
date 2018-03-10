b326aee95228e0baf48a9052567f0fbb54ec9687
Eviatar Bach <eviatarbach@protonmail.com>
2018-03-10 16:34:46 -0500

    Adding plot for error growth

---
+++ MAOOAM/plot_error.py
@@ -0,0 +1,31 @@
+from class_state_vector import state_vector
+from class_da_system import da_system
+import numpy as np
+from sys import argv
+import matplotlib.pyplot as plt
+
+nature_file ='x_nature.pkl'
+nature = state_vector()
+nature = nature.load(nature_file)
+
+freerun_file = 'x_freerun.pkl'
+freerun = state_vector()
+freerun = freerun.load(freerun_file)
+
+method = argv[1]
+analysis_file = 'x_analysis_{method}.pkl'.format(method=method)
+das = da_system()
+das = das.load(analysis_file)
+analysis = das.getStateVector()
+
+plt.plot(nature.getTimes(),
+         np.linalg.norm(freerun.getTrajectory() - nature.getTrajectory(),
+                        axis=1), label='Free run')
+plt.plot(nature.getTimes(),
+         np.linalg.norm(analysis.getTrajectory() - nature.getTrajectory(),
+                        axis=1), label='Analysis ({method})'.format(method=method))
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('Error', rotation='horizontal', labelpad=20)
+plt.tight_layout()
+plt.show()
