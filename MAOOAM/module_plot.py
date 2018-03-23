a654eb026c39637d05105d1e77eb5a316e79f1f0
Takuma Yoshida <takuma@github.noreply>
2018-03-23 11:14:57 -0400

    runs

--- MAOOAM/module_plot.py
+++ MAOOAM/module_plot.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+from class_state_vector import state_vector
+from class_da_system import da_system
 
 NDIM = 36
 
@@ -29,5 +31,30 @@ def __test_plot_time_colormap():
     sp.run("mkdir -p img", shell=True, check=True)
     plot_time_colormap(dat, "img/tmp.png", None, None, "test")
 
+def __sample_read_files():
+    for method in ["nudging", "ETKF"]:
+        nature_file ='x_nature.pkl'
+        nature = state_vector()
+        nature = nature.load(nature_file)
+        freerun_file = 'x_freerun.pkl'
+        freerun = state_vector()
+        freerun = freerun.load(freerun_file)
+        analysis_file = 'x_analysis_{method}.pkl'.format(method=method)
+        das = da_system()
+        das = das.load(analysis_file)
+        analysis = das.getStateVector()
+        plt.plot(nature.getTimes(),
+                 np.linalg.norm(freerun.getTrajectory() - nature.getTrajectory(),
+                                axis=1), label='Free run')
+        plt.plot(nature.getTimes(),
+                 np.linalg.norm(analysis.getTrajectory() - nature.getTrajectory(),
+                                axis=1), label='Analysis ({method})'.format(method=method))
+        plt.legend()
+        plt.xlabel('Time')
+        plt.ylabel('Error', rotation='horizontal', labelpad=20)
+        plt.tight_layout()
+        plt.savefig("img/tmp.png")
+        plt.close()
+
 if __name__ == "__main__":
-    __test_plot_time_colormap()
+    __sample_read_files()
